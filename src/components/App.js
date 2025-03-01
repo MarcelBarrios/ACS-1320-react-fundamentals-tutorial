3ffaeb5c5a90eed6c644497d899855bfd2bc8792
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-03-01 16:21:49 -0400

    extra challenges

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-import Title from './Title';
-import POPOSList from './POPOSList';
-import Footer from './Footer';
+import Title from './Title/Title';
+import POPOSList from './POPOSList/POPOSList';
+import Footer from './Footer/Footer';
 import { Outlet } from 'react-router-dom'
-import POPOSDetails from './POPOSDetails'
+import POPOSDetails from './POPOSDetails/POPOSDetails'
 
 function App() {
   return (
--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -9,14 +9,16 @@ function POPOSSpace(props) {
     const { name, image, address, hours, id } = props
     return (
         <div className="POPOSSpace">
-            <Link to={`/details/${id}`}>
+            <Link to={`/details/${id}`} className='POPOSSpace-title'>
                 {name}
             </Link>
             <Link to={`/details/${id}`}>
                 <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
             </Link>
-            <div>{address}</div>
-            <div><i>{hours}</i></div>
+            <div className="POPOSSpace-info">
+                <div>{address}</div>
+                <div><i>{hours}</i></div>
+            </div>
         </div>
     )
 }
